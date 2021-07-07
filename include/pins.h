d85f312c54eac037192eb9e84835a1b0dc64d2cf
Viet <viet_phuong.dinh@utt.fr>
2021-07-07 20:48:40 +0200

    Ajout des libs customs et code de test

---
+++ include/pins.h
@@ -0,0 +1,18 @@
+#ifndef PINS_H
+#define PINS_H
+#include "Arduino.h"
+
+#define SONAR_0_ECHO 3
+#define SONAR_0_TRIG 2
+#define SONAR_1_ECHO 5
+#define SONAR_1_TRIG 4
+#define SONAR_2_ECHO 7
+#define SONAR_2_TRIG 6
+#define SONAR_3_ECHO 9
+#define SONAR_3_TRIG 8
+#define SONAR_4_ECHO A1
+#define SONAR_4_TRIG A0
+#define SONAR_5_ECHO A3
+#define SONAR_5_TRIG A2
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+
+//includes :
+#include "pins.h"
+#include "params.h"
+
+//libs :
+//sonars :
+#include <HC_SR04.h>
+//#include <SonarArray.hpp>
+
+//CAN :
+#include <cpp_header_gen/serial.hpp>
+
+#include <Ultrasonic.h>
+
+// Sonars functions and objects
+void sendSonarsValueToCan();
+
+/*SonarArray sonarArray(NB_SONARS,
+  SONAR_TRIG_FRONT_L, SONAR_ECHO_FRONT_L,
+  SONAR_TRIG_FRONT_R, SONAR_ECHO_FRONT_R,
+  SONAR_TRIG_L, SONAR_ECHO_L,
+  SONAR_TRIG_R, SONAR_ECHO_R,
+  SONAR_TRIG_BACK, SONAR_ECHO_BACK
+);*/
+
+Ultrasonic sonar0(SONAR_TRIG_FRONT_L, SONAR_ECHO_FRONT_L);
+Ultrasonic sonar1(SONAR_TRIG_FRONT_R, SONAR_ECHO_FRONT_R);
+Ultrasonic sonar2(SONAR_TRIG_BACK_L, SONAR_ECHO_BACK_L);
+Ultrasonic sonar3(SONAR_TRIG_BACK_R, SONAR_ECHO_BACK_R);
+Ultrasonic sonar4(); //pins?
+Ultrasonic sonar5(); //pins?
+unsigned long lastMillis = 0;
+
+// Serial handler for frames coming from serial
+SerialHandler serialHandler;
+
+// OLED display (width, height, I2C, reset pin = -1 for sharing arduino reset pin)
+
+void setup() {
+  // Serial frames
+  serialHandler.begin(57600);
+
+}
+
+void loop() {
+  //sonarArray.update();
+
+  std::vector<int> frame = serialHandler.read();
+
+if (lastMillis + 200 < millis()) {
+  sendSonarsValueToCan();
+  lastMillis = millis();
+}
+
+
+void sendSonarsValueToCan()
+{
+  uint8_t measures[6] = {(uint8_t)sonar0.Ranging(CM), (uint8_t)sonar1.Ranging(CM), (uint8_t)sonar2.Ranging(CM), (uint8_t)sonar3.Ranging(CM),(uint8_t)sonar4.Ranging(CM),(uint8_t)sonar5.Ranging(CM) };
+  serialHandler.send(SONAR_DISTANCE, measures[0], measures[1], measures[2], measures[3], measures[4], measures[5]);
+
+  Serial.flush();
+}
