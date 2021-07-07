ed67eee38ec23236b90bbd9bbc424f43110edb9e
Florimond Lalande <38161801+Flowpy@users.noreply.github.com>
2021-07-08 01:44:17 +0200

    Changed to canHandler.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@
 //#include <SonarArray.hpp>
 
 //CAN :
-#include <cpp_header_gen/serial.hpp>
+#include <cpp_header_gen/can.hpp>
 
 #include <Ultrasonic.h>
 
@@ -29,36 +29,48 @@ Ultrasonic sonar0(SONAR_TRIG_FRONT_L, SONAR_ECHO_FRONT_L);
 Ultrasonic sonar1(SONAR_TRIG_FRONT_R, SONAR_ECHO_FRONT_R);
 Ultrasonic sonar2(SONAR_TRIG_BACK_L, SONAR_ECHO_BACK_L);
 Ultrasonic sonar3(SONAR_TRIG_BACK_R, SONAR_ECHO_BACK_R);
-Ultrasonic sonar4(SONAR_TRIG_RIGHT, SONAR_ECHO_RIGHT); 
-Ultrasonic sonar5(SONAR_TRIG_LEFT, SONAR_ECHO_LEFT); 
+Ultrasonic sonar4(SONAR_TRIG_LEFT, SONAR_ECHO_LEFT); 
+Ultrasonic sonar5(SONAR_TRIG_RIGHT, SONAR_ECHO_RIGHT); 
 unsigned long lastMillis = 0;
 
 // Serial handler for frames coming from serial
-SerialHandler serialHandler;
+CanHandler canHandler;
 
 // OLED display (width, height, I2C, reset pin = -1 for sharing arduino reset pin)
 
 void setup() {
   // Serial frames
-  serialHandler.begin(57600);
+  canHandler.setup();
+  Serial.begin(115200);
+  Serial.println("\n");
 
 }
 
 void loop() {
   //sonarArray.update();
 
-  std::vector<int> frame = serialHandler.read();
+  std::vector<int> frame = canHandler.read();
 
-if (lastMillis + 200 < millis()) {
-  sendSonarsValueToCan();
-  lastMillis = millis();
+  if (millis() - lastMillis > 200) {
+    sendSonarsValueToCan();
+    lastMillis = millis();
+  }
+  delay(1);
 }
 
 
 void sendSonarsValueToCan()
 {
   uint8_t measures[6] = {(uint8_t)sonar0.Ranging(CM), (uint8_t)sonar1.Ranging(CM), (uint8_t)sonar2.Ranging(CM), (uint8_t)sonar3.Ranging(CM),(uint8_t)sonar4.Ranging(CM),(uint8_t)sonar5.Ranging(CM) };
-  serialHandler.send(SONAR_DISTANCE, measures[0], measures[1], measures[2], measures[3], measures[4], measures[5]);
+  for (uint8_t i = 0; i < 6; i++) {
+    Serial.print(measures[i]);
+    Serial.print("\t");
+  }
+  Serial.println("\n");
 
-  Serial.flush();
+  canHandler.send(SONAR_DISTANCE, measures[0], measures[1], measures[2], measures[3], measures[4], measures[5]);
+
+  
+  //Serial.print(measures[3]);
+  canHandler.flush();
 }
